fa8cf047ec93e5892b98a3a2b15acc0f078652a2
mamleiva <martinl@exo.com.ar>
2021-07-07 17:53:03 -0300

    realizado hasta el ejercicio 3 de E/S

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,14 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+
+	nombre = prompt("Ingrese su Nombre:");
+	//nombre = prompt;
+
+
+	//let nombre = "Miguel";  //declaro la var nombre y la inicializo con Miguel
+    //nombre = "Juan" // esta es otra manera de declarar variables
+	alert(nombre);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,9 +3,17 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	let nombre;
+	nombre = document.getElementById("txtIdNombre").value; //esta es la manera estandar de Javascript
+	
+	/*esta es otra manera nombre = txtIdNombre.value; /esto es porque cada elemento que tiene un id en html, los ultimos 
+	navegadores como chrome, los guardan en una variable con el nombre del id */
+	
+	alert(nombre);
+
+	nombre = document.getElementById("txtIdNombre").value = "";
+
+	
 
 }
 
