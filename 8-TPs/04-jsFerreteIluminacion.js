a3bd6dee5a94b7f5ca9dc6c514644274c180efcc
mamleiva <martinl@exo.com.ar>
2021-07-16 13:04:04 -0300

    con los if optimizados, sin corregir pro profes aun

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,4 +1,7 @@
-/*4.	Para el departamento de iluminación:
+/*
+Nombre: Miguel
+Apellido: Leiva
+4.	Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
@@ -8,7 +11,74 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
+function CalcularPrecio() 
 {
- 	
+    //declaro vars    
+    let cantidadLamparas;
+    let precioNeto;
+    let precioBruto;
+    let marcaLampara;
+    let porcentaje;
+
+    //ingreso de datos
+    precioBruto = 35;
+    porcentaje = 0;
+    cantidadLamparas = parseInt(document.getElementById("txtIdCantidad").value);
+    marcaLampara = document.getElementById("Marca").value;
+
+    //condiciones
+    if (cantidadLamparas > 5) //A
+    {
+        porcentaje = 50;
+    }
+    else {
+        if (cantidadLamparas == 5) //B
+        {
+            if (marcaLampara == "ArgentinaLuz") {
+                porcentaje = 40;
+            }
+            else {
+                porcentaje = 30;
+            }
+
+        }
+        else {
+            if (cantidadLamparas == 4) //C
+            {
+                if (marcaLampara == "FelipeLamparas" || marcaLampara == "ArgentinaLuz") {
+                    porcentaje = 25;
+                }
+                else {
+                    porcentaje = 20;
+
+                }
+            }
+            if (cantidadLamparas == 3) //D
+            {
+                if (marcaLampara == "ArgentinaLuz") {
+                    porcentaje = 15;
+                }
+                else {
+                    if (marcaLampara == "FelipeLamparas") {
+                        porcentaje = 10;
+                    }
+                    else {
+                        porcentaje = 5;
+                    }
+                }
+            }
+        }
+    }
+
+    //calculo precio
+    precioNeto = (precioBruto - (precioBruto * porcentaje / 100)) * cantidadLamparas;
+
+    //calculo de IIBB. Para mas de $120, suma 10%
+    if (precioNeto > 120) 
+    {
+        precioNeto = (precioNeto + (precioNeto * 10 / 100));
+    }
+
+    //asigno precio final a box html
+    document.getElementById("txtIdprecioDescuento").value = precioNeto
 }
